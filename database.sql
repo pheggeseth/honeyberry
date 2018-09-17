f9c0bd5cc3367f1e9c7dbe537e05fea11e94b62c
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-17 13:11:53 -0500

    created store get route for user

--- database.sql
+++ database.sql
@@ -1,5 +1,63 @@
+-- the users of the app
 CREATE TABLE person (
     id SERIAL PRIMARY KEY,
     username VARCHAR (80) UNIQUE NOT NULL,
     password VARCHAR (1000) NOT NULL
+);
+
+-- shopping lists created by users
+CREATE TABLE store (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR (255) NOT NULL,
+	person_id INT NOT NULL REFERENCES "person"
+);
+
+-- categories that group similar items together (Fruits, Dairy, etc.)
+CREATE TABLE category (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR (255) NOT NULL
+);
+
+-- a library of items that may be added to shopping lists for any store (provided by default)
+CREATE TABLE item (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR (255) NOT NULL,
+	default_unit VARCHAR (255),
+	category_id INT NOT NULL REFERENCES "category",
+	image_path VARCHAR (255)
+);
+
+-- junction table for adding items to specific store lists
+CREATE TABLE store_item (
+	id SERIAL PRIMARY KEY,
+	index INT NOT NULL,
+	store_id INT NOT NULL REFERENCES "store",
+	item_id INT NOT NULL REFERENCES "item",
+	completed BOOLEAN NOT NULL
+);
+
+-- items for specific stores that the user consideres frequently bought "essentials"
+CREATE TABLE store_essential (
+	id SERIAL PRIMARY KEY,
+	store_id INT NOT NULL REFERENCES "store",
+	item_id INT NOT NULL REFERENCES "item"
+);
+
+-- item groups created by users representing "areas" in a store, such as the produce section, aisle 5, etc
+-- these areas are ordered for each store list and represent the order in which items added to the main
+-- shopping list for this store should be sorted
+CREATE TABLE area (
+	id SERIAL PRIMARY KEY,
+	index INT NOT NULL,
+	name VARCHAR (255) NOT NULL,
+	person_id INT NOT NULL REFERENCES "person",
+	store_id INT NOT NULL REFERENCES "store"
+);
+
+-- items belonging to specific areas
+CREATE TABLE area_item (
+	id SERIAL PRIMARY KEY,
+	index INT NOT NULL,
+	area_id INT NOT NULL REFERENCES "area",
+	item_id INT NOT NULL REFERENCES "item"
 );
--- server/routes/store.router.js
+++ server/routes/store.router.js
@@ -7,25 +7,35 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-    
+  if (req.isAuthenticated()) {
+    const queryText = `SELECT "id", "name" FROM "store" WHERE "person_id" = $1;`;
+    pool.query(queryText, [req.user.id])
+    .then(response => res.send(response.rows))
+    .catch(error => {
+      console.log('/api/store GET error for user id:', req.user.id);
+      res.sendStatus(500);
+    });
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 /**
  * POST route template
  */
 router.post('/', (req, res) => {
-  // if (req.isAuthenticated()) {
+  if (req.isAuthenticated()) {
     const newStore = req.body;
     const queryText = `INSERT INTO "store" ("name", "person_id") VALUES ($1, $2);`;
-    pool.query(queryText, [newStore.name, newStore.userId])
+    pool.query(queryText, [newStore.name, req.user.id])
     .then(() => res.sendStatus(200))
     .catch(error => {
       console.log('/api/store POST error:', error);
       res.sendStatus(500);
     });
-  // } else {
-  //   res.sendStatus(401);
-  // }
+  } else {
+    res.sendStatus(401);
+  }
 });
 
 module.exports = router;
