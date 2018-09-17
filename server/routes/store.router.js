b1b26112f9ff33c0fe67ae3f5b4b43a63cf07b1c
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-17 13:28:40 -0500

    added store delete route

--- server/routes/store.router.js
+++ server/routes/store.router.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
-
 // "/api/store"
 /**
  * GET route template
@@ -25,12 +24,27 @@ router.get('/', (req, res) => {
  */
 router.post('/', (req, res) => {
   if (req.isAuthenticated()) {
+    console.log('user', req.user);
     const newStore = req.body;
     const queryText = `INSERT INTO "store" ("name", "person_id") VALUES ($1, $2);`;
     pool.query(queryText, [newStore.name, req.user.id])
     .then(() => res.sendStatus(200))
     .catch(error => {
       console.log('/api/store POST error:', error);
+      res.sendStatus(500);
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+router.delete('/', (req, res) => {
+  if (req.isAuthenticated()) {
+    const queryText = `DELETE FROM "store" WHERE "id" = $1 AND "person_id" = $2;`;
+    pool.query(queryText, [req.query.id, req.user.id])
+    .then(() => res.send(200))
+    .catch(error => {
+      console.log('/api/store DELETE error for user id:', req.user.id, error);
       res.sendStatus(500);
     });
   } else {
