7fd77c31f067e0db90ad99019bdb16fbba7d71e7
Paul Heggeseth <pheggeseth@gmail.com>
2018-09-21 20:16:13 -0500

    separated edit list menu to own component

---
+++ src/components/ListPage/ListEditMenu/ListEditMenu.js
@@ -0,0 +1,99 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import styled from 'styled-components';
+
+import { CURRENT_STORE_ACTIONS } from '../../../redux/actions/currentStoreActions';
+
+const ClickAway = styled.div`
+  width: 100%;
+  height: 100%;
+  background-color: transparent;
+  position: fixed;
+  top: 0;
+`;
+
+const mapStateToProps = state => ({
+  editingList: state.currentStore.editingList,
+  selectingItems: state.currentStore.selectingItems,
+  selectedItems: state.currentStore.selectedItems,
+  currentStore: state.currentStore.store,
+  movingItems: state.currentStore.movingItems,
+  stores: state.userStores,
+});
+
+class ListEditMenu extends Component {
+  toggleListEditMode = () => {
+    const {dispatch} = this.props;
+    if (this.props.selectingItems) {
+      dispatch({type: CURRENT_STORE_ACTIONS.CLEAR_SELECTED_ITEMS});
+      dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_SELECTION_MODE});
+    }
+    dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_LIST_EDITING_MODE});
+  };
+
+  startListEditSelect = () => {
+    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_SELECTION_MODE});
+    this.props.dispatch({type: CURRENT_STORE_ACTIONS.SET_SELECTED_ITEMS, payload: []});
+  };
+
+  stopListEditSelect = () => {
+    this.props.dispatch({type: CURRENT_STORE_ACTIONS.CLEAR_SELECTED_ITEMS});
+    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_SELECTION_MODE});
+  };
+
+  startListItemMoveMode = () => {
+    console.log('now moving items:', this.props.selectedItems);
+    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_MOVE_MODE});
+  };
+
+  stopListItemMoveMode = () => {
+    console.log('stopping moving items:', this.props.selectedItems);
+    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_MOVE_MODE});
+  };
+
+  deleteListItems = () => {
+    console.log('deleting items:', this.props.selectedItems);
+    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_DELETE_MODE});
+  };
+
+  moveSelectedItemsToStore = storeId => () => {
+    console.log('moving items to store with id '+storeId, this.props.selectedItems);
+  };
+
+  render() {
+    const {editingList, selectingItems, currentStore, movingItems, stores} = this.props;
+    let editListButtons = <div><button onClick={this.toggleListEditMode}>Edit List</button></div>;
+    if (editingList && !selectingItems) {
+      editListButtons = (
+        <div>
+          <div style={{zIndex: 101, position: 'relative'}}>
+            <button onClick={this.toggleListEditMode}>Back</button>
+            <button onClick={this.startListEditSelect}>Select</button>
+            <button>Share</button>
+          </div>
+          <ClickAway onClick={this.toggleListEditMode} />
+        </div>
+      );
+    } else if (editingList && selectingItems && !movingItems) {
+      editListButtons = (
+        <div>
+          <button onClick={this.stopListEditSelect}>Back</button>
+          <button onClick={this.startListItemMoveMode}>Move</button>
+          <button onClick={this.deleteListItems}>Delete</button>
+        </div>
+      );
+    } else if (editingList && movingItems) {
+      editListButtons = (
+        <div>
+          <button onClick={this.stopListItemMoveMode}>Back</button>
+          {stores.filter(store => store.id !== currentStore.id)
+          .map(store => <button key={store.id} onClick={this.moveSelectedItemsToStore(store.id)}>{store.name}</button>)}
+        </div>
+      );
+    }
+
+    return (editListButtons);
+  }
+}
+
+export default connect(mapStateToProps)(ListEditMenu);
--- src/components/ListPage/ListPage.js
+++ src/components/ListPage/ListPage.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 import Nav from '../../components/Nav/Nav';
 import { USER_ACTIONS } from '../../redux/actions/userActions';
@@ -14,29 +14,22 @@ import CurrentItems from './CurrentItems/CurrentItems';
 import CompletedItems from './CompletedItems/CompletedItems';
 import EssentialItems from './EssentialItems/EssentialItems';
 import ItemsAll from '../ItemsAll/ItemsAll';
-
-const ClickAway = styled.div`
-  width: 100%;
-  height: 100%;
-  background-color: transparent;
-  position: fixed;
-  top: 0;
-`;
+import ListEditMenu from './ListEditMenu/ListEditMenu';
 
 const mapStateToProps = state => ({
   user: state.user,
   items: state.items,
   categories: state.categories,
-  stores: state.userStores,
+  // stores: state.userStores,
   currentStore: state.currentStore.store,
   list: state.currentStore.list,
   essentials: state.currentStore.essentials,
   searching: state.itemSearch.searching,
   editingItem: state.currentStore.editingItem,
-  editingList: state.currentStore.editingList,
-  selectingItems: state.currentStore.selectingItems,
-  selectedItems: state.currentStore.selectedItems,
-  movingItems: state.currentStore.movingItems,
+  // editingList: state.currentStore.editingList,
+  // selectingItems: state.currentStore.selectingItems,
+  // selectedItems: state.currentStore.selectedItems,
+  // movingItems: state.currentStore.movingItems,
 });
 
 class ListPage extends Component {
@@ -73,77 +66,11 @@ class ListPage extends Component {
     }
   }
 
-  toggleListEditMode = () => {
-    const {dispatch} = this.props;
-    if (this.props.selectingItems) {
-      dispatch({type: CURRENT_STORE_ACTIONS.CLEAR_SELECTED_ITEMS});
-      dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_SELECTION_MODE});
-    }
-    dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_LIST_EDITING_MODE});
-  };
 
-  startListEditSelect = () => {
-    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_SELECTION_MODE});
-    this.props.dispatch({type: CURRENT_STORE_ACTIONS.SET_SELECTED_ITEMS, payload: []});
-  };
-
-  stopListEditSelect = () => {
-    this.props.dispatch({type: CURRENT_STORE_ACTIONS.CLEAR_SELECTED_ITEMS});
-    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_SELECTION_MODE});
-  };
-
-  startListItemMoveMode = () => {
-    console.log('now moving items:', this.props.selectedItems);
-    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_MOVE_MODE});
-  };
-
-  stopListItemMoveMode = () => {
-    console.log('stopping moving items:', this.props.selectedItems);
-    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_MOVE_MODE});
-  };
-
-  deleteListItems = () => {
-    console.log('deleting items:', this.props.selectedItems);
-    this.props.dispatch({type: CURRENT_STORE_ACTIONS.TOGGLE_ITEM_DELETE_MODE});
-  };
-
-  moveSelectedItemsToStore = storeId => () => {
-    console.log('moving items to store with id '+storeId, this.props.selectedItems);
-  };
 
   render() {
     let content = null;
-    const {list, searching, editingItem, essentials, user, editingList, selectingItems, currentStore, movingItems, stores} = this.props;
-
-    let editListButtons = <div><button onClick={this.toggleListEditMode}>Edit List</button></div>;
-    if (editingList && !selectingItems) {
-      editListButtons = (
-        <div>
-          <div style={{zIndex: 101, position: 'relative'}}>
-            <button onClick={this.toggleListEditMode}>Back</button>
-            <button onClick={this.startListEditSelect}>Select</button>
-            <button>Share</button>
-          </div>
-          <ClickAway onClick={this.toggleListEditMode} />
-        </div>
-      );
-    } else if (editingList && selectingItems && !movingItems) {
-      editListButtons = (
-        <div>
-          <button onClick={this.stopListEditSelect}>Back</button>
-          <button onClick={this.startListItemMoveMode}>Move</button>
-          <button onClick={this.deleteListItems}>Delete</button>
-        </div>
-      );
-    } else if (editingList && movingItems) {
-      editListButtons = (
-        <div>
-          <button onClick={this.stopListItemMoveMode}>Back</button>
-          {stores.filter(store => store.id !== currentStore.id)
-          .map(store => <button key={store.id} onClick={this.moveSelectedItemsToStore(store.id)}>{store.name}</button>)}
-        </div>
-      );
-    }
+    const {list, searching, editingItem, essentials, user} = this.props;
 
     if (editingItem) {
       content = (
@@ -173,7 +100,7 @@ class ListPage extends Component {
       <div>
         <Nav />
         {/* {JSON.stringify(list)} */}
-        {editListButtons}
+        <ListEditMenu />
         { user.userName ? content : null }
       </div>
     );
